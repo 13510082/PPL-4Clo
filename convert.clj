1ae571a7717ff082193147fa053ffe48800e2fb4
dimas-angga <dimas11@rocketmail.com>
2014-02-26 18:31:36 +0700

    .

---
+++ convert.clj
@@ -0,0 +1,314 @@
+(html->hiccup "<html class=\"no-js\"><head><meta charset=\"utf-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\"><title>4Clojure - Welcome!</title><meta name=\"description\" content=\"\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" type=\"text/css\" href=\"css/isotope.css\" media=\"screen\" /><link rel=\"stylesheet\" href=\"js/fancybox/jquery.fancybox.css\" type=\"text/css\" media=\"screen\" /><link rel=\"stylesheet\" href=\"css/bootstrap.css\"><link rel=\"stylesheet\" href=\"css/bootstrap-theme.css\"><link rel=\"stylesheet\" href=\"css/style.css\"><link href=\"/css/style__v2.0.0-rc1.css\" rel=\"stylesheet\" type=\"text/css\" /></head><body><section id=\"header\" class=\"appear\"></section><div class=\"navbar navbar-fixed-top\" role=\"navigation\" data-0=\"line-height:100px; height:100px; background-color:rgba(0,0,0,0.3);\" data-300=\"line-height:60px; height:60px; background-color:rgba(0,0,0,1);\"><div class=\"container\"><div class=\"navbar-header\"><button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\".navbar-collapse\"><span class=\"fa fa-bars color-white\"></span></button><h1><a class=\"navbar-brand\" href=\"index.html\" data-0=\"line-height:90px;\" data-300=\"line-height:20px;\"><img src = \"img/4Clo-Logo.png\"></img></a></h1></div><div class=\"navbar-collapse collapse\"><ul class=\"nav navbar-nav\" data-0=\"margin-top:20px;\" data-300=\"margin-top:5px;\"><li class=\"active\"><a href=\"index.html\">Home</a></li><li><a href=\"#section-contribute\">Contribute</a></li><li><a href=\"#section-user\">Top User</a></li><li><a href=\"#section-problems\">Problem List</a></li><li><a href=\"#section-register\">Register</a></li><li><a href=\"http://tryclj.com/\">REPL</a><li><li><a href=\"http://clojure.org/\">Docs</a><li></ul></div></div></div><section class=\"featured\"><div class=\"container\"> <h1><a class=\"navbar-brand\" href=\"index.html\" data-0=\"line-height:90px;\" data-300=\"line-height:20px;\"></a></h1><div class=\"row mar-bot40\"><div class=\"align-left\"><h2 class=\"slogan\">Welcome to 4Clojure</h2><p>4Clojure is a resource to help fledgling clojurians learn the language through interactive problems.<br>The first few problems are easy enough that even someone with no prior experience should find the learning curve forgiving.<br>See 'Help' for more information.</p><br><input type=\"text\" name=\"username\" class=\"form-control\" id=\"username\" placeholder=\"Username\"  data-rule=\"maxlen:4\" data-msg=\"Please enter at least 4 chars\" /><input type=\"text\" name=\"password\" class=\"form-control\" id=\"password\" placeholder=\"Password\"  data-rule=\"maxlen:8\" data-msg=\"Please enter at least 8 chars\" /><button type=\"submit\" class=\"btn btn-theme pull-left\">LOGIN</button><br><br><p>Doesn't have account yet? <a href=\"#section-contact\"> Register </a> now!</p><img src = \"img/Facebook-Login.png\"></img> or <img src = \"img/Twitter-Login.png\"></img></div></div></div></section><section id=\"section-contribute\" class=\"section pad-bot30 bg-white\"><div class=\"container\"> <div class=\"align-center\"><h1 class=\"text-bold\">Let's Contribute!</h1>Is this site written in Clojure?Absolutely! This site was created using a variety of open source Clojure (and Java) libraries. In fact, the code for this site is itself open source. Once you've mastered the language, feel free to contribute something back to the community.<br></br><div id=\"github-banner\"><a alt=\"Fork 4Clojure on Github!\" href=\"https://github.com/4Clojure/4Clojure\"><img src= \"img/ForkGitHub.png\"></a></div></div>	</div></section><section id=\"testimonials\" class=\"section\" data-stellar-background-ratio=\"0.5\"><div class=\"container\"><div class=\"row\"><div class=\"col-lg-12\"><div class=\"align-center\"><img src=\"img/flipCounter-medium.png\"></img><div class=\"testimonial pad-top40 pad-bot40 clearfix\"><h5>Clojure approaches solving software problems from a different angle than classical object-oriented techniques, but it does so having been motivated by their fundamental strengths and shortcomings</h5><br/>
+											<span class=\"author\">&mdash; MICHAEL FOGUS <a href=\"#\">http://www.drdobbs.com/architecture-and-design/the-clojure-philosophy/240150710</a></span>
+										</div>
+
+								</div>
+							</div>
+					</div>
+				
+			</div>	
+		</div>	
+		</section>
+			
+				<section id=\"section-user\" class=\"section appear clearfix\">
+		<div class=\"container\">
+
+				<div class=\"row mar-bot40\">
+					<div class=\"col-md-offset-3 col-md-6\">
+						<div class=\"section-header\">
+							<h2 class=\"section-heading animated\" data-animation=\"bounceInUp\">Top User</h2>
+							<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet consectetur, adipisci velit, sed quia non numquam.</p>
+						</div>
+					</div>
+				</div>
+
+					<div class=\"row align-center mar-bot40\">
+						<div class=\"col-md-3\">
+							<div class=\"team-member\">
+								<figure class=\"member-photo\"><img src=\"img/team/member1.jpg\" alt=\"\" /></figure>
+								<div class=\"team-detail\">
+									<h4>Jason Doe</h4>
+									<span>User experiences</span>
+								</div>
+							</div>
+						</div>
+						<div class=\"col-md-3\">
+							<div class=\"team-member\">
+								<figure class=\"member-photo\"><img src=\"img/team/member2.jpg\" alt=\"\" /></figure>
+								<div class=\"team-detail\">
+									<h4>Timothy Clark</h4>
+									<span>Web developer</span>
+								</div>
+							</div>
+						</div>
+						<div class=\"col-md-3\">
+							<div class=\"team-member\">
+								<figure class=\"member-photo\"><img src=\"img/team/member3.jpg\" alt=\"\" /></figure>
+								<div class=\"team-detail\">
+								<h4>Vicky Tan</h4>
+									<span>Web designer</span>
+								</div>
+							</div>
+						</div>
+						<div class=\"col-md-3\">
+							<div class=\"team-member\">
+								<figure class=\"member-photo\"><img src=\"img/team/member4.jpg\" alt=\"\" /></figure>
+								<div class=\"team-detail\">
+								<h4>Kevin Peterson</h4>
+									<span>UI designer</span>
+								</div>
+							</div>
+						</div>
+					</div>
+						
+		</div>
+		</section>
+				<section id=\"parallax1\" class=\"section pad-top40 pad-bot40\" data-stellar-background-ratio=\"0.5\">
+			<div class=\"container\">
+            <div class=\"align-center pad-top40 pad-bot40\">
+                <blockquote class=\"bigquote color-white\">\"Simplicity, freedom to focus, empowerment, consistency, and clarity: Nearly every element of the Clojure programming language is designed to promote these goals\"</blockquote>
+				<p class=\"color-white\">Michael Fogus</p>
+            </div>
+			</div>	
+		</section>
+		
+				<section id=\"section-problems\" class=\"section appear clearfix\">
+			<div class=\"container\">
+				
+				<div class=\"row mar-bot40\">
+					<div class=\"col-md-offset-3 col-md-6\">
+						<div class=\"section-header\">
+							<h2 class=\"section-heading animated\" data-animation=\"bounceInUp\">Problem List</h2>
+							<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet consectetur, adipisci velit, sed quia non numquam.</p>
+						</div>
+					</div>
+				</div>
+					
+                        <div class=\"row\">
+                          <nav id=\"filter\" class=\"col-md-12 text-center\">
+                            <ul>
+                              <li><a href=\"#\" class=\"current btn-theme btn-small\" data-filter=\"*\">All</a></li>
+                              <li><a href=\"#\"  class=\"btn-theme btn-small\" data-filter=\".webdesign\" >Web Design</a></li>
+                              <li><a href=\"#\"  class=\"btn-theme btn-small\" data-filter=\".photography\">Photography</a></li>
+                              <li ><a href=\"#\" class=\"btn-theme btn-small\" data-filter=\".print\">Print</a></li>
+                            </ul>
+                          </nav>
+                          <div class=\"col-md-12\">
+                            <div class=\"row\">
+                              <div class=\"portfolio-items isotopeWrapper clearfix\" id=\"3\">
+							  
+                                <article class=\"col-md-4 isotopeItem webdesign\">
+									<div class=\"portfolio-item\">
+										<img src=\"img/portfolio/img1.jpg\" alt=\"\" />
+										 <div class=\"portfolio-desc align-center\">
+											<div class=\"folio-info\">
+												<h5><a href=\"#\">Portfolio name</a></h5>
+												<a href=\"img/portfolio/img1.jpg\" class=\"fancybox\"><i class=\"fa fa-plus fa-2x\"></i></a>
+											 </div>										   
+										 </div>
+									</div>
+                                </article>
+
+                                <article class=\"col-md-4 isotopeItem photography\">
+									<div class=\"portfolio-item\">
+										<img src=\"img/portfolio/img2.jpg\" alt=\"\" />
+										 <div class=\"portfolio-desc align-center\">
+											<div class=\"folio-info\">
+												<h5><a href=\"#\">Portfolio name</a></h5>
+												<a href=\"img/portfolio/img2.jpg\" class=\"fancybox\"><i class=\"fa fa-plus fa-2x\"></i></a>
+											 </div>										   
+										 </div>
+									</div>
+                                </article>
+								
+
+                                <article class=\"col-md-4 isotopeItem photography\">
+									<div class=\"portfolio-item\">
+										<img src=\"img/portfolio/img3.jpg\" alt=\"\" />
+										 <div class=\"portfolio-desc align-center\">
+											<div class=\"folio-info\">
+												<h5><a href=\"#\">Portfolio name</a></h5>
+												<a href=\"img/portfolio/img3.jpg\" class=\"fancybox\"><i class=\"fa fa-plus fa-2x\"></i></a>
+											 </div>										   
+										 </div>
+									</div>
+                                </article>
+
+                                <article class=\"col-md-4 isotopeItem print\">
+									<div class=\"portfolio-item\">
+										<img src=\"img/portfolio/img4.jpg\" alt=\"\" />
+										 <div class=\"portfolio-desc align-center\">
+											<div class=\"folio-info\">
+												<h5><a href=\"#\">Portfolio name</a></h5>
+												<a href=\"img/portfolio/img4.jpg\" class=\"fancybox\"><i class=\"fa fa-plus fa-2x\"></i></a>
+											 </div>										   
+										 </div>
+									</div>
+                                </article>
+
+                                <article class=\"col-md-4 isotopeItem photography\">
+									<div class=\"portfolio-item\">
+										<img src=\"img/portfolio/img5.jpg\" alt=\"\" />
+										 <div class=\"portfolio-desc align-center\">
+											<div class=\"folio-info\">
+												<h5><a href=\"#\">Portfolio name</a></h5>
+												<a href=\"img/portfolio/img5.jpg\" class=\"fancybox\"><i class=\"fa fa-plus fa-2x\"></i></a>
+											 </div>										   
+										 </div>
+									</div>
+                                </article>
+
+                                <article class=\"col-md-4 isotopeItem webdesign\">
+									<div class=\"portfolio-item\">
+										<img src=\"img/portfolio/img6.jpg\" alt=\"\" />
+										 <div class=\"portfolio-desc align-center\">
+											<div class=\"folio-info\">
+												<h5><a href=\"#\">Portfolio name</a></h5>
+												<a href=\"img/portfolio/img6.jpg\" class=\"fancybox\"><i class=\"fa fa-plus fa-2x\"></i></a>
+											 </div>										   
+										 </div>
+									</div>
+                                </article>
+
+                                <article class=\"col-md-4 isotopeItem print\">
+									<div class=\"portfolio-item\">
+										<img src=\"img/portfolio/img7.jpg\" alt=\"\" />
+										 <div class=\"portfolio-desc align-center\">
+											<div class=\"folio-info\">
+												<h5><a href=\"#\">Portfolio name</a></h5>
+												<a href=\"img/portfolio/img7.jpg\" class=\"fancybox\"><i class=\"fa fa-plus fa-2x\"></i></a>
+											 </div>										   
+										 </div>
+									</div>
+                                </article>
+
+                                <article class=\"col-md-4 isotopeItem photography\">
+									<div class=\"portfolio-item\">
+										<img src=\"img/portfolio/img8.jpg\" alt=\"\" />
+										 <div class=\"portfolio-desc align-center\">
+											<div class=\"folio-info\">
+												<h5><a href=\"#\">Portfolio name</a></h5>
+												<a href=\"img/portfolio/img8.jpg\" class=\"fancybox\"><i class=\"fa fa-plus fa-2x\"></i></a>
+											 </div>										   
+										 </div>
+									</div>
+                                </article>
+
+                                <article class=\"col-md-4 isotopeItem print\">
+									<div class=\"portfolio-item\">
+										<img src=\"img/portfolio/img9.jpg\" alt=\"\" />
+										 <div class=\"portfolio-desc align-center\">
+											<div class=\"folio-info\">
+												<h5><a href=\"#\">Portfolio name</a></h5>
+												<a href=\"img/portfolio/img9.jpg\" class=\"fancybox\"><i class=\"fa fa-plus fa-2x\"></i></a>
+											 </div>										   
+										 </div>
+									</div>
+                                </article>
+                                </div>
+                                        
+							</div>
+                                     
+
+							</div>
+                        </div>
+				
+			</div>
+		</section>
+		<section id=\"parallax2\" class=\"section parallax\" data-stellar-background-ratio=\"0.5\">	
+            <div class=\"align-center pad-top40 pad-bot40\">
+                <blockquote class=\"bigquote color-black\">\"Clojure was born out of frustration provoked in large part by the complexities of concurrent programming, complicated by the weaknesses of object-oriented programming in facilitating it.\" </blockquote>
+				<p class=\"color-white\">Michael Fogus</p>
+            </div>
+		</section>
+
+				<section id=\"section-register\" class=\"section appear clearfix\">
+			<div class=\"container\">
+				
+				<div class=\"row mar-bot40\">
+					<div class=\"col-md-offset-3 col-md-6\">
+						<div class=\"section-header\">
+							<h2 class=\"section-heading animated\" data-animation=\"bounceInUp\">Register</h2>
+													</div>
+					</div>
+				</div>
+				<div class=\"row\">
+					<div class=\"col-md-8 col-md-offset-2\">
+						<div class=\"cform\" id=\"contact-form\">
+							<div id=\"sendmessage\">
+								 You have been registered. Thank you!
+							</div>
+							<form action=\"contact/contact.php\" method=\"post\" role=\"form\" class=\"contactForm\">
+							  <div class=\"form-group\">
+								<label for=\"name\">Username</label>
+								<input type=\"text\" name=\"name\" class=\"form-control\" id=\"name\" placeholder=\"Userame\" data-rule=\"maxlen:4\" data-msg=\"Please enter at least 4 chars\" />
+								<div class=\"validation\"></div>
+							  </div>
+							  <div class=\"form-group\">
+								<label for=\"password\">Password</label>
+								<input type=\"text\" name=\"password\" class=\"form-control\" id=\"password\" placeholder=\"Password\" data-rule=\"maxlen:7\" data-msg=\"Please enter at least 7 chars\" />
+								<div class=\"validation\"></div>
+							  </div>
+							  <div class=\"form-group\">
+								<label for=\"repeatpassword\">Repeat Password</label>
+								<input type=\"text\" name=\"repeatpassword\" class=\"form-control\" id=\"repeatpassword\" placeholder=\"Repeat Password\" data-rule=\"maxlen:7\" data-msg=\"Please enter at least 7 chars\" />
+								<div class=\"validation\"></div>
+							  </div>
+							  <div class=\"form-group\">
+								<label for=\"email\">Email</label>
+								<input type=\"email\" class=\"form-control\" name=\"email\" id=\"email\" placeholder=\"Email\" data-rule=\"email\" data-msg=\"Please enter a valid email\" />
+								<div class=\"validation\"></div>
+							  </div>
+							  
+							  
+							  <button type=\"submit\" class=\"btn btn-theme pull-left\">Register</button>
+							</form>
+
+						</div>
+					</div>
+					
+				</div>
+				
+			</div>
+		</section>
+		
+	<section id=\"footer\" class=\"section footer\">
+		<div class=\"container\">
+			<div class=\"row animated opacity mar-bot20\" data-andown=\"fadeIn\" data-animation=\"animation\">
+				<div class=\"col-sm-12 align-center\">
+                    <ul class=\"social-network social-circle\">
+                        <li><a href=\"#\" class=\"icoRss\" title=\"Rss\"><i class=\"fa fa-rss\"></i></a></li>
+                        <li><a href=\"#\" class=\"icoFacebook\" title=\"Facebook\"><i class=\"fa fa-facebook\"></i></a></li>
+                        <li><a href=\"#\" class=\"icoTwitter\" title=\"Twitter\"><i class=\"fa fa-twitter\"></i></a></li>
+                        <li><a href=\"#\" class=\"icoGoogle\" title=\"Google +\"><i class=\"fa fa-google-plus\"></i></a></li>
+                        <li><a href=\"#\" class=\"icoLinkedin\" title=\"Linkedin\"><i class=\"fa fa-linkedin\"></i></a></li>
+                    </ul>				
+				</div>
+			</div>
+
+			<div class=\"row align-center copyright\">
+					<div class=\"col-sm-12\"><p>Copyright &copy; 2014 Amoeba - by <a href=\"http://bootstraptaste.com\">Bootstraptaste</a></p></div>
+			</div>
+		</div>
+
+	</section>
+	<a href=\"#header\" class=\"scrollup\"><i class=\"fa fa-chevron-up\"></i></a>	
+
+	<script src=\"js/modernizr-2.6.2-respond-1.1.0.min.js\"></script>
+	<script src=\"js/jquery.js\"></script>
+	<script src=\"js/jquery.easing.1.3.js\"></script>
+    <script src=\"js/bootstrap.min.js\"></script>
+	<script src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyASm3CwaK9qtcZEWYa-iQwHaGi3gcosAJc&sensor=false\"></script>
+	<script src=\"js/jquery.isotope.min.js\"></script>
+	<script src=\"js/jquery.nicescroll.min.js\"></script>
+	<script src=\"js/fancybox/jquery.fancybox.pack.js\"></script>
+	<script src=\"js/skrollr.min.js\"></script>		
+	<script src=\"js/jquery.scrollTo-1.4.3.1-min.js\"></script>
+	<script src=\"js/jquery.localscroll-1.2.7-min.js\"></script>
+	<script src=\"js/stellar.js\"></script>
+	<script src=\"js/jquery.appear.js\"></script>
+	<script src=\"js/validate.js\"></script>
+    <script src=\"js/main.js\"></script>
+        
+	</body>
+</html>")
