622c5b2b3234da3ac57334f3c3591ec2301ceeb8
dimas-angga <dimas11@rocketmail.com>
2014-03-01 09:04:09 +0700

    Last version

--- 4clojure/src/foreclojure/template.clj
+++ 4clojure/src/foreclojure/template.clj
@@ -15,66 +15,96 @@
   (let [attrs (rendering-info (page-attributes body))
         user (session/get :user)]
     (html
-     (doctype :html5)
+     (doctype :html)
      [:html
-      [:head
-       [:title (:title attrs)]
-       [:link {:rel "alternate" :type "application/atom+xml" :title "Atom" :href "/problems/rss"}]
+   [:head
+      [:meta {:charset "utf-8"}]
+      [:meta {:content "IE=edge,chrome=1", :http-equiv "X-UA-Compatible"}]
+      [:title "4Clojure - Welcome!"]
+      [:meta {:content "", :name "description"}]
+      [:meta {:content "width=device-width, initial-scale=1.0", :name "viewport"}]
+      [:link {:media "screen", :href "css/isotope.css", :type "text/css", :rel "stylesheet"}]
+      [:link {:media "screen", :type "text/css", :href "js/fancybox/jquery.fancybox.css", :rel "stylesheet"}]
+      [:link {:href "css/bootstrap.css", :rel "stylesheet"}]
+      [:link {:href "css/bootstrap-theme.css", :rel "stylesheet"}]
+      [:link {:href "css/style.css", :rel "stylesheet"}]
+      [:link {:type "text/css", :rel "stylesheet", :href "/css/style__v2.0.0-rc1.css"}]
+      [:link {:rel "alternate" :type "application/atom+xml" :title "Atom" :href "/problems/rss"}]
        [:link {:rel "shortcut icon" :href (static-url "favicon2.ico")}]
-       (css "css/style.css" "css/demo_table.css" "css/codemirror.css")
+       (css "css/demo_table.css" "css/codemirror.css")
        (apply css (map #(format "css/theme/%s.css" %) codemirror-themes))
        (js "vendor/script/jquery-1.5.2.min.js" "vendor/script/jquery.dataTables.min.js" "vendor/script/jquery.flipCounter.1.1.pack.js" "vendor/script/jquery.easing.1.3.js" "vendor/script/jquery.dataTables.fnSetFilteringDelay.js")
        (js "script/codebox.js" "script/foreclojure.js")
        (js "vendor/script/codemirror-clojure-compressed.js")
        (js "vendor/script/detectmobilebrowser.js")
        (javascript-tag (format "CodeBox.theme = '%s';" (get-theme)))]
-      [:body
-       (when (:fork-banner attrs)
-         [:div#github-banner [:a {:href repo-url
-                                  :alt "Fork 4Clojure on Github!"}]])
-       [:div#top
-        (link-to "/" [:img#logo {:src (static-url "images/4clj-logo-small.png")
-                                 :alt "4clojure.com"}])]
-       [:div#content
-        [:div#menu
-         (for [[link text & [tabbed]]
-               [["/" "Main Page"]
-                ["/problems" "Problem List"]
-                ["/users" "Top Users"]
-                ["/directions" "Help"]
-                ["http://try-clojure.org" "REPL" true]
-                ["http://clojuredocs.org" "Docs" true]]]
-           [:a.menu (assoc (when tabbed {:target "_blank"})
-                      :href link)
-            text])
-         [:div#user-info
-          (if user
-            [:div
-             [:span#username (str "Logged in as " user)]
-             [:a#logout {:href "/logout"} "Logout"]]
-            [:div
-             [:a#login {:href (login-url)} "Login"]
-             [:a#register {:href "/register"} "Register"]])]]
-        (when user
-          [:div#lower-menu
-           [:span
-            (link-to "/settings" "Account Settings")]
-           (when (:golfing-active config)
-             [:span ; deserves its own page, but just make it discoverable for now
-              (link-to "/league" "Leagues")])
-           (when (approver? user)
-             [:span
-              (link-to "/problems/unapproved" "View Unapproved Problems")])
-           (when (can-submit? user)
-             [:span (link-to "/problems/submit" "Submit a Problem")])])
-        [:div#content_body (:content attrs)]
-        [:div#footer
-         "The content on 4clojure.com is available under the EPL v 1.0 license."
-         (let [email "team@4clojure.com"]
-           [:span
-            [:a#contact {:href (str "mailto:" email)} "Contact us"]
-            (str " (" email ")")])]
-        (javascript-tag
+   [:body
+      [:section {:class "appear", :id "header"}]
+      [:div
+         {:data-300 "line-height:60px; height:60px; background-color:rgba(0,0,0,1);", :data-0 "line-height:100px; height:100px; background-color:rgba(0,0,0,0.3);", :role "navigation", :class "navbar navbar-fixed-top"}
+         [:div {:class "container"}
+            [:div {:class "navbar-header"}
+               [:button {:data-target ".navbar-collapse", :data-toggle "collapse", :class "navbar-toggle", :type "button"}
+                  [:span {:class "fa fa-bars color-white"}]]
+               [:h1
+                  [:a {:data-300 "line-height:20px;", :data-0 "line-height:90px;", :href "index.html", :class "navbar-brand"}
+                     [:img {:src "img/4Clo-Logo.png"}]]]]
+            [:div {:class "navbar-collapse collapse"}
+
+             [:ul {:data-300 "margin-top:5px;", :data-0 "margin-top:20px;", :class "nav navbar-nav"}
+                  [:li {:class "active"}
+                     [:a {:href "#"} "Home"]]
+                  [:li
+                     [:a {:href "#section-contribute"} "Contribute"]]
+                  [:li
+                     [:a {:href "/users"} "Top User"]]
+                  [:li
+                     [:a {:href "http://localhost:3001/problems"} "Problem List"]]
+                  [:li
+                     [:a {:href "#section-register"} "Register"]]
+                  [:li
+                     [:a {:href "http://tryclj.com/"} "REPL"]]
+                  [:li]
+                  [:li
+                     [:a {:href "http://clojure.org/"} "Docs"]]
+                  [:li]]]]]
+        [:section {:class "featured"}
+           [:div {:class "container"}
+              [:h1
+                 [:a {:data-300 "line-height:20px;", :data-0 "line-height:90px;", :href "index.html", :class "navbar-brand"}]]
+              [:div {:class "row mar-bot40"}
+                 [:div {:class "align-left"}
+                    [:h2 {:class "slogan"} "Welcome to 4Clojure"]
+                    [:p "4Clojure is a resource to help fledgling clojurians learn the language through interactive problems." [:br] "The first few problems are easy enough that even someone with no prior experience should find the learning curve forgiving." [:br] "See 'Help' for more information."]
+                    [:br]
+                    [:input {:data-msg "Please enter at least 4 chars", :data-rule "maxlen:4", :placeholder "Username", :id "username", :class "form-control", :name "username", :type "text"}]
+                    [:input {:data-msg "Please enter at least 8 chars", :data-rule "maxlen:8", :placeholder "Password", :id "password", :class "form-control", :name "password", :type "text"}]
+                    [:button {:class "btn btn-theme pull-left", :type "submit"} "LOGIN"]
+                    [:br]
+                    [:br]
+                    [:p "Doesn't have account yet? " [:a {:href "#section-contact"} " Register "] " now!"]
+                    [:img {:src "img/Facebook-Login.png"}]
+                    [:img {:src "img/Twitter-Login.png"}]]]]]
+        [:section {:class "section pad-bot30 bg-white", :id "section-contribute"}
+           [:div {:class "container"}
+              [:div {:class "align-center"}
+                 [:h1 {:class "text-bold"} "Let's Contribute!"]
+                 [:p"Is this site written in Clojure?Absolutely! This site was created using a variety of open source Clojure (and Java) libraries. In fact, the code for this site is itself open source. Once you've mastered the language, feel free to contribute something back to the community."]
+                 [:br]
+                 [:div {:id "github-banner"}
+                        [:a {:href "https://github.com/4Clojure/4Clojure", :alt "Fork 4Clojure on Github!"} [:img {:src "img/ForkGitHub.png"}]]]]]]
+      [:section {:data-stellar-background-ratio "0.5", :class "section", :id "testimonials"}
+           [:div {:class "container"}
+              [:div {:class "row"}
+                 [:div {:class "col-lg-12"}
+                    [:div {:class "align-center"}
+                         [:img {:src "img/flipCounter-medium.png"}]
+                         [:div {:class "testimonial pad-top40 pad-bot40 clearfix"}
+                              [:h5 "Clojure approaches solving software problems from a different angle than classical object-oriented techniques, but it does so having been motivated by their fundamental strengths and shortcomings"]
+                              [:br]
+                              [:span {:class "author"} "— MICHAEL FOGUS " [:a {:href "#"} "http://www.drdobbs.com/architecture-and-design/the-clojure-philosophy/240150710"]]]]]]]]
+
+      (javascript-tag
          "var _gaq = _gaq || [];
           _gaq.push(['_setAccount', 'UA-22844856-1']);
           _gaq.push(['_trackPageview']);
@@ -84,7 +114,13 @@
             ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
           })();"
-         )]]])))
+         )
+    ]]
+)))
+
+
+
+
 
 ;; Content templates
 (defn content-page [{:keys [heading heading-note sub-heading main]}]
